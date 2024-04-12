74a2a1e0812471bdaf8619223879f618603e3db5
Yash Jain <113338423+yashjain3010@users.noreply.github.com>
2024-04-12 09:29:43 +0530

    Add files via upload

---
+++ Day-04/Tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { RevenueCard } from './component/RevenueCard';
+
+function App() {
+
+  return (
+    <div className=''>
+     <RevenueCard title={"Amount Pending"} Amount={"92100.20"} orderCount={"13"}/>
+    </div>
+  );
+}
+
+export default App

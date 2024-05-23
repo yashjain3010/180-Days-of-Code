bc182f867ec31cf2d41d9ecc4a2cbc99f72b2970
Yash Jain <113338423+yashjain3010@users.noreply.github.com>
2024-05-23 10:18:05 +0530

    Add files via upload

---
+++ Day-30/src/components/Error.js
@@ -0,0 +1,6 @@
+import React from 'react'
+const Error = () => {
+  return <div className='section section-center'><h2>there was an error...</h2></div>
+}
+
+export default Error
---
+++ Day-30/src/pages/AboutPage.js
@@ -0,0 +1,57 @@
+import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
+import aboutImg from "../assets/hero-bcg.jpeg";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <PageHero title="About" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            At Comfy Sloth, our mission is to provide top-quality Products. We strive to create an online shopping
+            experience that is simple, enjoyable, and personalized to meet your
+            needs.We are committed to sustainability and ethical sourcing,
+            ensuring that our products are both high-quality and responsibly
+            produced.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
+export default AboutPage;
